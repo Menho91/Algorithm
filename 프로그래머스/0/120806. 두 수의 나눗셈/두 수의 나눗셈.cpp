9e7d44e2dd25e4b059f27b5ba985be170c2f54e3
Menho <82595532+Menho91@users.noreply.github.com>
2024-12-26 10:32:59 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    float temp = (static_cast<float>(num1) / static_cast<float>(num2)) * 1000;
+    int answer = temp;
+    return answer;
+}
