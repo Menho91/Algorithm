5348e5eaca5b672c2a0dba0747a29c352124c545
Menho <82595532+Menho91@users.noreply.github.com>
2024-12-27 12:16:51 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    for(int i = sqrt(n) - 1; i < sqrt(n) + 2; i++)
+    {
+        if(pow(i, 2) == n)
+            return pow(i+1, 2);
+    }
+    answer = -1;
+    return answer;
+}
