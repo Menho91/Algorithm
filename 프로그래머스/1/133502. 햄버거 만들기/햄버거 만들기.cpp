94c94569411537571dc37adf1ce023dbcbc3457d
Menho91 <82595532+Menho91@users.noreply.github.com>
2025-05-01 09:40:24 +0900

    [level 1] Title: 햄버거 만들기, Time: 15.01 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int solution(vector<int> ingredient) {
+    int answer = 0;
+    vector<int> burger;
+    for(int ingred : ingredient)
+    {
+        burger.push_back(ingred);
+        if(burger[burger.size() - 1] == 1 && burger[burger.size() - 2] == 3 && burger[burger.size() - 3] == 2 && burger[burger.size() - 4] == 1)
+        {
+            answer++;
+            for(int i = 0; i < 4; i++)
+            {
+                burger.pop_back();
+            }
+        }
+    }
+    return answer;
+}
